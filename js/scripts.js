b8d3998dccb23a6a22c2c8b81084036e8c6c2eb9
jessicakenney <jessicakenney@yahoo.com>
2017-07-24 23:44:02 -0700

    experiment with determining div id that was clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 
+var values = ["a","b","c"];
+var shuffledValues = ["b","c","a"];
+//createBoard();
+// for i =0 to < shuffledValues.length
+//   var newTile = new Tile (shuffledValue,id)
+
+
   $(".col-md-4").click(function() {
     $(this).append("X");
+    var id = event.target.id;
+    console.log("id= "+ id);
   });
 
 });
